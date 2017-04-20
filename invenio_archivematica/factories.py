5248a741d7167d6ba734ea3b4aa7e30bebf9dc97
Rémi Ducceschi <remi.ducceschi@gmail.com>
2017-04-20 16:50:51 +0200

    API: functions to archive a record
    - Adds functions to archive a record
    - Adds support for not archivable records

--- invenio_archivematica/factories.py
+++ invenio_archivematica/factories.py
@@ -52,22 +52,29 @@ def transfer_cp(uuid, destination):
         It needs to be a absolute path to a folder
     """
     record = Record.get_record(uuid)
-    pid = PersistentIdentifier.get_by_object("recid", "rec", uuid)
+    pid = PersistentIdentifier.get_by_object('recid', 'rec', uuid)
     dir_name = join(destination,
                     create_accessioned_id(pid.pid_value, 'recid'))
     try:
         mkdir(dir_name)
-    except:
+    except OSError:
         pass
     for fileobj in record.files:
         copyfile(fileobj.file.storage().fileurl,
                  join(dir_name, fileobj.key))
 
 
-def is_archivable(record):
+def is_archivable_all(record):
     """Tell if the given record should be archived or not.
 
     If this function returns True, the record will be archived later.
     Otherwise, the record will never get archived.
+
+    This function archive all the records.
     """
     return True
+
+
+def is_archivable_none(record):
+    """Archive no records."""
+    return False
